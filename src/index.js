d385606a85a0ff7244d9e6b1b320c8375ec954cc
Kseniya <33444745+KseniyaMirosha@users.noreply.github.com>
2021-02-10 14:11:41 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  module.exports = function check(str, bracketsConfig) {
+    bracketsConfig = {
+        '[': ']',
+        '(': ')',
+        '<': '>',
+        '|': '|',
+    };
+
+    let result = str.split('').reduce(function (acc, curr) {
+
+        if (~Object.keys(bracketsConfig).indexOf(curr)) {
+            acc.push(curr);
+        }
+
+        if (~Object.values(bracketsConfig).indexOf(curr) && bracketsConfig[acc[acc.length - 1]] === curr) {
+            acc.length = --acc.length;
+        }
+
+        return acc;
+    }, []);
+
+    return !Boolean(result.length);
+};
